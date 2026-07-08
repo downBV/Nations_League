13e954345996b2e8335d98da94bc907c22bf0c29
downBV <bvik159@gmail.com>
2026-07-08 09:53:58 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Football Manager 2026 - Service Worker
 // Verzió növelése frissítéskor kényszeríti az újratöltést
-const CACHE_VERSION = 'fm2026-v12'; // v12: csapat-szintű előretolási vonal (középpályások/védők felnyomulnak támadáskor)
+const CACHE_VERSION = 'fm2026-v13'; // v12: csapat-szintű előretolási vonal (középpályások/védők felnyomulnak támadáskor)
 const CACHE_NAME = `${CACHE_VERSION}-cache`;
 
 // Fájlok amiket cache-elünk offline használathoz
